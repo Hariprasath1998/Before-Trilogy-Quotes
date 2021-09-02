e91635cef00351bc22e40abc261d49d41de757c2
Hari <hari@MacBooks-MacBook-Pro.local>
2021-09-02 13:51:43 +0530

    Added previous button

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Quote(props) {
+    const quote = props.quote;
     return (
         <div className="container">
-            <div id="quote">{props.quote.Quote}</div>
-            <div id="author">- {props.quote.Author}</div>
+            <div id="quote">{quote.Quote}</div>
+            <div id="author">- {quote.Author}</div>
         </div>
     )
 }
